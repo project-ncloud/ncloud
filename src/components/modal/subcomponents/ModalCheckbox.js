ca46ec3c0f8fb3b74d1a3764dabbd7d4b6461b4b
Sourav Gain <souravgain605@gmail.com>
2021-02-17 13:44:23 +0530

    Added new component for modal ModalCheckbox

---
+++ src/components/modal/subcomponents/ModalCheckbox.js
@@ -0,0 +1,26 @@
+import { React, useState } from "react";
+
+function ModalCheckbox({ name, data, icon, func, key }) {
+  return (
+    <>
+      <div className="row checkboxRow">
+        <label htmlFor={key}>
+          <div className="labelInfo">
+            {icon}
+            {name}
+          </div>
+          <input
+            type="checkbox"
+            name={key}
+            id={key}
+            value={data}
+            checked={Boolean(data)}
+            onClick={func}
+          ></input>
+        </label>
+      </div>
+    </>
+  );
+}
+
+export default ModalCheckbox;
