65179b003dc6fd1e8417586a023f5f2ea3e61e5f
Sourav Gain <souravgain605@gmail.com>
2021-02-23 10:55:55 +0530

    use ncloud btn of explorer for going back to drive panel

--- src/components/FileManagerComponents/SideBar.js
+++ src/components/FileManagerComponents/SideBar.js
@@ -1,13 +1,19 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import { useHistory } from "react-router-dom";
 
 function SideBar({ func }) {
+  const history = useHistory();
   const explorerConstants = useSelector(
     (state) => state.explorerControlReducer
   );
+
+  const goBackHome = () => {
+    history.goBack();
+  };
   return (
     <div className="fSideBar">
-      <i className="ri-hard-drive-fill"></i>
+      <i className="ri-hard-drive-fill" onClick={() => goBackHome()}></i>
       <i
         className={`ri-add-line ${
           !explorerConstants.writable ? "disabled" : null
@@ -23,6 +29,7 @@ function SideBar({ func }) {
         className={`ri-share-forward-line ${
           !explorerConstants.admin ? "disabled" : null
         }`}
+        onClick={() => {}}
       ></i>
     </div>
   );
